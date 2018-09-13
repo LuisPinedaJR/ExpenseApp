70dacbc87d354e7a78ffaf4997e5751540ad54b1
Luis Pineda JR <lspnd00@gmail.com>
2018-09-12 23:07:40 -0400

    more destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,18 +1,30 @@
-const person = {
-	// name: 'Luis',
-	age: 30,
-	location: {
-		city: 'Tampa',
-		temp: 92
+// const person = {
+// 	// name: 'Luis',
+// 	age: 30,
+// 	location: {
+// 		city: 'Tampa',
+// 		temp: 92
+// 	}
+// };
+
+// const {name: firstName = 'Joe Dirt', age} = person;
+
+// console.log(`${firstName} is ${age}`);
+
+// const {city, temp: temperature } = person.location;
+
+// if(city && temperature){
+// 	console.log(`it's ${temperature} in ${city}.`);
+// }
+
+const book = {
+	title: 'ego is the enemy',
+	author: 'Ryan Holiday',
+	publisher: {
+		name: 'Penguin'
 	}
 };
 
-const {name: firstName = 'Joe Dirt', age} = person;
+const { name: publisherName = 'Self-published '} = book.publisher;
 
-console.log(`${firstName} is ${age}`);
-
-const {city, temp: temperature } = person.location;
-
-if(city && temperature){
-	console.log(`it's ${temperature} in ${city}.`);
-}
+console.log(publisherName); // Penguin, Self-published
