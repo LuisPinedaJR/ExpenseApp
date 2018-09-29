5ee9a0767e8927b024d94df73abe0f319776624e
Luis Pineda JR <lspnd00@gmail.com>
2018-09-28 20:12:50 -0400

    fixed bug and added reset and setcount redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -12,6 +12,16 @@ const decrementCount = ({ decrementBy = 1} = {}) => ({
 	decrementBy
 });
 
+const setCount = ({ count }) => ({
+	type: 'SET',
+	count
+});
+
+const resetCount = () => ({
+	type: 'RESET',
+});
+
+
 const store = createStore((state = { count: 0 }, action) => {
 	switch (action.type) {
 	case 'INCREMENT':
@@ -41,28 +51,19 @@ const unsubscribe = store.subscribe(() => {
 	console.log(store.getState());
 });
 
-// store.dispatch({
-// 	type: 'INCREMENT',
-// 	incrementBy: 5
-// });
 
 store.dispatch(incrementCount({ incrementBy: 5 }));
 
 store.dispatch(incrementCount());
 
-store.dispatch({
-	type: 'INCREMENT'
-});
 
-store.dispatch({
-	type: 'RESET'
-});
+
+store.dispatch(resetCount());
 
 store.dispatch(decrementCount());
 
+
+
 store.dispatch(decrementCount({ decrementBy: 10}));
 
-store.dispatch({
-	type: 'SET',
-	count: 101
-});
+store.dispatch(setCount({ count: 101 }));
